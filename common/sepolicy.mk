07715e5181d12f67d0670fd79fafb57425becfec
Bauuuuu <abansode@outlook.com>
2018-06-06 00:08:58 +0200

    Revert "sepolicy: common: add rules for aosp vibrator hal"
    This reverts commit 9dc11afd9d1a9138894bfb0843f42ba7cc601884.
    
    Change-Id: I133bc9f28764adb63b350e75489f688e1d12c6a7

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -8,6 +8,3 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aosp/sepolicy/common/private
-
-BOARD_SEPOLICY_DIRS += \
-    device/aosp/sepolicy/common/vendor
